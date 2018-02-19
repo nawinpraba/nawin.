77f85eef50c40d105d0b341bbde45444e780dcdc
nawinpraba <35774842+nawinpraba@users.noreply.github.com>
2018-02-19 13:19:01 +0530

    Create ov.c

---
+++ ov.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main(void) 
+{
+           int a[100],n,i,j,temp;
+           int median=0;
+           printf("enter the n values");
+           scanf("%d",&n);
+           for(i=0;i<n;i++)
+           scanf("%d",&a[i]);
+           for(i=0;i<n;i++)
+           {
+           	for(j=i+1;j<n;j++)
+           	{
+           		if(a[j]<a[i])
+           		{
+           	               temp=a[j];
+           	               a[j]=a[i];
+           	               a[i]=temp;
+           		}
+           	}
+           
+           }
+          if(n%2==0) {
+        
+        median=((a[n/2] + a[n/2 - 1]) / 2.0);
+        printf("median value is %d",median);
+    } else {
+      
+        median=a[n/2];
+        printf("median value is %d",median);
+    }
+          
+           
+	return 0;
+}
