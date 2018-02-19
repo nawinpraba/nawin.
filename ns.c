a425573db57c536a68c4d19bb2d157f929bb6356
nawinpraba <35774842+nawinpraba@users.noreply.github.com>
2018-02-19 13:33:59 +0530

    Create ns.c

---
+++ ns.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include<string.h>
+int main(void) {
+	char a[10];
+	int i,l,count=0;
+	printf("enter the values");
+	scanf("%s",a);
+	l=strlen(a);
+	for(i=0;i<l;i++)
+	{
+		if(a[i]>='0' && a[i]<='9')
+		{
+		count++;
+		}
+  }
+  if(count==0)
+  printf("No");
+  else
+  printf("Yes");
+	return 0;
+}
