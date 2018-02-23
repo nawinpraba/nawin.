a404d42abecc6738fcd9d652bba55566c999bc42
nawinpraba <35774842+nawinpraba@users.noreply.github.com>
2018-02-23 16:23:34 +0530

    Create msdn14.c

---
+++ msdn14.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main(void)
+{
+    int a,b;
+    printf("enter the 1st num");
+    scanf("%d",&a);
+    printf("enter the 2nd num");
+    scanf("%d",&b);
+    printf("sum of the num is %d",a+b);
+    return 0;
+}
