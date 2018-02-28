8b1a6cb1920bd4f45b3cc88c8754070bc0a9c316
nawinpraba <35774842+nawinpraba@users.noreply.github.com>
2018-02-28 04:29:45 -0500

    Create msdn26.c

---
+++ msdn26.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int i;
+	char s[100],a_count=0,n_count=0;
+	gets(s);
+	for(i=0;s[i]!='\0';i++)
+	{
+		if((s[i]>='a' && s[i]<='z') || (s[i]>='A' && s[i]<='Z'))
+		{
+			a_count++;
+		}
+		if(s[i]>='0' && s[i]<='9')
+		{
+			n_count++;
+		}
+	}
+	if((a_count && n_count)>0)
+	printf(" yes");
+	else
+	printf("no");
+}
+
+
+
+
