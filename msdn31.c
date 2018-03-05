e4a1addc52be4706b3537aba4d9c00b496ef94a2
nawinpraba <35774842+nawinpraba@users.noreply.github.com>
2018-03-05 13:21:41 +0530

    Create msdn31.c

---
+++ msdn31.c
@@ -0,0 +1,38 @@
+
+#include <stdio.h>
+#include<conio.h>
+
+
+
+int main(void) 
+
+{
+
+	char str[50];
+
+	int i,count=1;
+
+	printf("enter the sentence:");
+
+	scanf("%[^\n]s",str);
+
+	for(i=0;str[i]!='\0';i++)
+
+	{
+
+		if(str[i]==' ')
+
+		{
+
+			count=count+1;
+
+		}
+
+	}
+
+	printf("\n no of words=%d",count);
+
+	return 0;
+
+}
+
