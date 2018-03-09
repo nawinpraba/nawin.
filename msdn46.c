3037202c31c9e29dea90919b1c71a9d36bbc30e0
nawinpraba <35774842+nawinpraba@users.noreply.github.com>
2018-03-09 16:10:27 +0530

    Create msdn46.c

---
+++ msdn46.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main(void)
+{
+    int a;
+    scanf("%d",&a);
+    if(a%13==0)
+{
+    printf("yes");
+}
+else
+{
+    printf("no");
+}
+return 0;
+
+    
+}
